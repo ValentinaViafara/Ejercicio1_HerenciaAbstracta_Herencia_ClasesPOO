257ae4777c3c25916037ccdf7de4c767c5ebd0cd
Estudiante <Estudiante@168.176.36.180>
2018-04-04 09:51:51 -0400

    Quiz POO

---
+++ src/Lider.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+
+public class Lider extends Programador {
+    
+    
+    private ArrayList<Programador> programadores;
+
+    public Lider(int salarioMensual, String lenguajeConocido, int id, String nombre) {
+        super(salarioMensual, lenguajeConocido, id, nombre);
+        
+        
+    }
+    
+    
+}
